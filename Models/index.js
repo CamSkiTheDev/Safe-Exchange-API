34c71a62c1e62e1048e36392d08cad67712fbcdd
Cameron Lucas <cameronlucasaim@gmail.com>
2021-05-25 17:55:17 -0600

    fixes relationships

---
+++ Models/index.js
@@ -0,0 +1,50 @@
+const { Schema, model, Types } = require("mongoose");
+///////////////////////////////
+// MODELS
+////////////////////////////////
+const VideoSchema = new Schema(
+  {
+    URL: String,
+    log: {
+      type: Types.ObjectId,
+      ref: "Log",
+    },
+  },
+  { timestamps: true }
+);
+
+const LogSchema = new Schema(
+  {
+    Time: String,
+    Geoposition: String,
+    isActive: Boolean,
+    videos: [
+      {
+        type: Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const UserSchema = new Schema(
+  {
+    userid: String,
+    name: String,
+    email: String,
+    log: [
+      {
+        type: Types.ObjectId,
+        ref: "Log",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = model("User", UserSchema);
+const Log = model("Log", LogSchema);
+const Video = model("Video", VideoSchema);
+
+module.exports = { User, Log, Video };
