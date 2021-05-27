c16d6f212ce2c8aad238f862b36dddfd5dfb4c7f
Pasha Brown <pashabrown@gmail.com>
2021-05-26 18:03:07 -0700

    Video.js update

--- Routes/Video.js
+++ Routes/Video.js
@@ -1,30 +1,30 @@
-const {User} = require("../Models");
+const {Video} = require("../Models");
 const { Router } = require("express");
 const router = Router();
 
 //index route
 router.get("/", async (req, res) => {
-  res.json(await User.find({}).populate("log"));
+  res.json(await Video.find({}));
 });
 
 //create route
 router.post("/", async (req, res) => {
-  res.json(await User.create(req.body));
+  res.json(await Video.create(req.body));
 });
 
 //update route
 router.put("/:id", async (req, res) => {
-  res.json(await User.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+  res.json(await Video.findByIdAndUpdate(req.params.id, req.body, { new: true }));
 });
 
 //delete route
 router.delete("/:id", async (req, res) => {
-  res.json(await User.findByIdAndRemove(req.params.id));
+  res.json(await Video.findByIdAndRemove(req.params.id));
 });
 
 //Show Route 
 router.get("/:id", async (req, res) =>{
-  res.json(await User.findOne({userid:req.params.id}).populate("log"));
+  res.json(await Video.findOne(req.params.id));
 });
 
 // EXPORT ROUTER
